8f5b5a3197d869405b5d5db39943d31f329adec7
anandh-05 <anandhsivan05@gmail.com>
2026-06-01 21:30:14 +0530

    fixed github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/wedding-card/',
-  plugins: [react(), tailwindcss()],
+  base: '/sreelal-akshaya-wedding-card/',
+  plugins: [react(),],
 })
